70ad5407df73b262daeb7b4c0b69e22588585ba9
JonHannah <jon.a.hannah@gmail.com>
2017-07-05 13:58:55 +0100

    Set up watch task and chart module

---
+++ frontend/src/js/chart.js
@@ -0,0 +1,67 @@
+/* globals dates, blend, priceIndices */
+
+var chart = (function() {
+
+  var init = function() {
+    var compositeIndex = buildComposite(dates, blend, priceIndices);
+    generate(compositeIndex);
+  };
+
+  var buildComposite = function(dates, blend, priceIndices){
+    var compositeIndex = [];
+    compositeIndex.length = dates.length;
+    compositeIndex.fill(0);
+    compositeIndex[0] = 'Blend';
+
+    blend.forEach(function(element){
+      index = priceIndices[element.display].usd;
+
+      for (var i = 1; i < index.length; i++) {
+        compositeIndex[i] += element.percentage * index[i];
+      }
+    });
+
+    return compositeIndex;
+  };
+
+  var generate = function(compositeIndex) {
+    c3.generate({
+      data: {
+        x: 'date',
+        columns: [
+          dates,
+          priceIndices['Wheat'].usd,
+          priceIndices['Corn'].usd,
+          compositeIndex
+        ]
+      },
+      axis: {
+        x: {
+          type: 'timeseries',
+          tick: {
+            format: '%b \'%y'
+          }
+        },
+        y: {
+          tick: {
+            format: d3.format('.1f')
+          }
+        }
+      },
+      tooltip: {
+        format: {
+          value: function (value) {
+              var format = d3.format('.2f');
+              return format(value);
+            }
+        }
+      }
+    });
+
+  };
+
+  return {
+      init: init,
+  };
+
+})();
