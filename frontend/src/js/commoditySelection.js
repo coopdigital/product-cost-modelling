e45d5c12c2b6b22a676bc4ceec952a564d2af3aa
JonHannah <jon.a.hannah@gmail.com>
2017-07-17 13:23:37 +0100

    Tidy up commodity selection init function

--- frontend/src/js/commoditySelection.js
+++ frontend/src/js/commoditySelection.js
@@ -17,41 +17,45 @@ var commoditySelection = (function() {
       isModified = true;
     });
 
-    dismiss.click(function() {
-      // commodities must reflect latest DOM state
-      commodities = $('#commoditySelection').find('.commodity');
+    dismiss.click(dismissHandler);
 
-      commodities
-        .filter('.btn-success')
-        .not('.displayed')
-        .removeClass('btn-success active');
-
-      commodities
-        .filter('.displayed')
-        .addClass('btn-success active');
-
-      isModified = false;
-    });
-
-    apply.click(function() {
-      // commodities must reflect latest DOM state
-      commodities = $('#commoditySelection').find('.commodity');
-
-      modal.modal('hide');
-      $('#wrapper').removeClass('hidden');
-
-      if (isModified) {
-        commodities.removeClass('displayed');
-        commodities.filter('.btn-success').addClass('displayed');
-        recipe.init();
-      }
-
-      isModified = false;
-    });
+    apply.click(applyHandler);
 
     modify.click(function() {
       modal.modal('show');
     });
+  };
+
+  var dismissHandler = function() {
+    // commodities must reflect latest DOM state
+    commodities = $('#commoditySelection').find('.commodity');
+
+    commodities
+      .filter('.btn-success')
+      .not('.displayed')
+      .removeClass('btn-success active');
+
+    commodities
+      .filter('.displayed')
+      .addClass('btn-success active');
+
+    isModified = false;
+  };
+
+  var applyHandler = function() {
+    // commodities must reflect latest DOM state
+    commodities = $('#commoditySelection').find('.commodity');
+
+    modal.modal('hide');
+    $('#wrapper').removeClass('hidden');
+
+    if (isModified) {
+      commodities.removeClass('displayed');
+      commodities.filter('.btn-success').addClass('displayed');
+      recipe.init();
+    }
+
+    isModified = false;
   };
 
   return {
